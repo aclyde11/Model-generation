d0665522aae48b3e19c134465f2f3e1c418c2a07
Austin Clyde <aclyde@uchicago.edu>
2020-03-02 13:01:56 -0600

    covid branch

--- impress_md/interface_functions.py
+++ impress_md/interface_functions.py
@@ -41,10 +41,10 @@ def RunDocking(smiles, inpath, outpath, padding=4):
     # oedepict.OERenderMolecule(f'{outpath}/lig.png',lig)
 
 
-def RunDocking_(smiles, inpath, outpath, padding=4):
+def RunDocking_(smiles, inpath, outpath, padding=4, write=False):
     from . import conf_gen
     from . import dock_conf
-    if not os.path.exists(outpath):
+    if write and not os.path.exists(outpath):
         os.mkdir(outpath)
     confs = conf_gen.SelectEnantiomer(conf_gen.FromString(smiles))
     # This receptor can be pre-compiled to an oeb. It speeds things up
@@ -60,10 +60,11 @@ def RunDocking_(smiles, inpath, outpath, padding=4):
     #   the best pose, as scored by Openeye. It may be useful to consider
     #   something about the range of poses.
 
-    with open(f'{outpath}/metrics.csv','w+') as metrics:
-        metrics.write("Dock,Dock_U\n")
-        metrics.write("{},{}\n".format(dock_conf.BestDockScore(dock,lig),0))
-    dock_conf.WriteStructures(receptor, lig, f'{outpath}/apo.pdb', f'{outpath}/lig.pdb')
+    if write:
+        with open(f'{outpath}/metrics.csv','w+') as metrics:
+            metrics.write("Dock,Dock_U\n")
+            metrics.write("{},{}\n".format(dock_conf.BestDockScore(dock,lig),0))
+        dock_conf.WriteStructures(receptor, lig, f'{outpath}/apo.pdb', f'{outpath}/lig.pdb')
     # # If you uncomment the three lines below, it will save an image of the 2D
     #   molecule. This is useful as a sanity check.
     # from openeye import oedepict
