3fa31bae7ea0ca57635bb61bd3f953760f42a0e6
Austin Clyde <aclyde@uchicago.edu>
2020-03-02 11:23:44 -0600

    covid branch

--- runner.py
+++ runner.py
@@ -3,7 +3,7 @@ import pandas as pd
 import sys
 import policy
 from impress_md import interface_functions
-
+import time
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 
@@ -13,21 +13,28 @@ def setup_server():
 
     dockPolicy = policy.DockPolicy()
     mmPolicy = policy.MinimizePolicy()
+    print("Master setup server.")
+    ts = time.time()
+    docked_count = 0
+    param_count = 0
     while True:
         
         data = comm.recv(source=MPI.ANY_SOURCE, tag=MPI.ANY_TAG, status=status_)
         if len(data) == 1: #pipeline 1
             res = dockPolicy(*data)
             comm.send(int(res), dest=status_.Get_source(), tag=11)
+            docked_count += 1
         elif len(data) == 2: #pipeline 2
             res = mmPolicy(*data)
             comm.send(int(res), dest=status_.Get_source(), tag=11)
+            param_count += 1
         elif len(data) == 3: #pipline 3
             res = policy.mmgbsa_ns_policy(data[0], data[1], data[2])
             comm.send(int(res), dest=status_.Get_source(), tag=11)
         else:
             print("got some weird data", data)
-            
+        if time.time() - ts > 100:
+            print("current counts", docked_count, param_count)
 
 def worker(df):
     struct = "input/"
@@ -41,7 +48,7 @@ def worker(df):
         # pipline
         comm.send([smiles], dest=0, tag=11)
         r = comm.recv(source=0, tag=11)
-        print("Rank", rank, "should I run docking on", smiles,"?", "\t my model says", bool(r))
+        # print("Rank", rank, "should I run docking on", smiles,"?", "\t my model says", bool(r))
 
         # pipeline
         if r:
@@ -49,22 +56,22 @@ def worker(df):
             score = interface_functions.RunDocking_(smiles,struct,path)
             comm.send([smiles, score], dest=0, tag=11)
             r = comm.recv(source=0, tag=11)
-            print("Rank", rank, "should I run minimize, given the docking score", score, "?", "\t my model says", bool(r))
+            # print("Rank", rank, "should I run minimize, given the docking score", score, "?", "\t my model says", bool(r))
         
             # pipeline
             if r:
-                print("Rank", rank, "running param and mini")
+                # print("Rank", rank, "running param and mini")
                 interface_functions.ParameterizeOE(path)
                 mscore = interface_functions.RunMinimization_(path, path)
             
             
                 comm.send([smiles, score, mscore], dest=0, tag=11)
                 r = comm.recv(source=0, tag=11)
-                print("Rank", rank, "should I run mmgbsa for 1 ns given a energy minmization result of", mscore, "?\t my model says", bool(r))
+                # print("Rank", rank, "should I run mmgbsa for 1 ns given a energy minmization result of", mscore, "?\t my model says", bool(r))
                 if r:
-                    print("Rank", rank, "running simulation")
+                    # print("Rank", rank, "running simulation")
                     escore = interface_functions.RunMMGBSA_(path,path)
-                    print("Rank", rank, "ran simulation and got", escore)
+                    # print("Rank", rank, "ran simulation and got", escore)
         
 
 
