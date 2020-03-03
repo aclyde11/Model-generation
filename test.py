c1d56c0fb38e3fe4379114a3fa97533ad724c5f7
Austin Clyde <aclyde@uchicago.edu>
2020-03-03 10:06:05 -0600

    covid branch

--- test.py
+++ test.py
@@ -8,11 +8,11 @@ import subprocess
 df = pd.read_csv("input/john_smiles_kinasei.smi", sep=' ')
 
 struct = "input/"
-docker = interface_functions.get_receptr()
+docker, recept = interface_functions.get_receptr()
 
 for pos in range(0, 20):
     path = "test" + str(pos)  + "/"
     smiles = df.iloc[pos,0]
-    score = interface_functions.RunDocking_(smiles,struct,path, dock_obj=docker)
+    score = interface_functions.RunDocking_(smiles,struct,path, dock_obj=docker, recept=recept)
     interface_functions.ParameterizeOE(path)
     mscore = interface_functions.RunMinimization_(path, path)
