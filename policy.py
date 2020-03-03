9e0f7ab04653f8f9486039ae6c11f1e1c983bc58
Austin Clyde <aclyde@uchicago.edu>
2020-03-03 17:31:54 -0600

    covid branch

--- policy.py
+++ policy.py
@@ -1,31 +1,81 @@
+import random
 import numpy as np
 
-class DockPolicy():
+class MasterDockPolicy():
     def __init__(self):
+        pass
+
+    def rollout(self):
+        return None
+
+    def collect_rollout(self, buffer):
         pass
 
     def __call__(self, smile):
         return True
 
-class MinimizePolicy():
+class DockPolicy():
     def __init__(self):
-        self.buffer = np.zeros(10)
-        self.buffer_sat = True
-        self.pos = 0
-        
+        pass
+
+    def rollout(self):
+        return None
+
+    def collect_rollout(self, buffer):
+        pass
+
+    def __call__(self, smile):
+        return True
+
+class MasterMinimizePolicy:
+    def __init__(self, max=100000):
+        self.buffer = []
+
+    def __call__(self, rollout):
+        self.buffer.append(rollout)
+        return np.mean(self.buffer)
+
+
+class MinimizePolicy:
+    def __init__(self, max=100000):
+        self.buffer = []
+        self.cutoff = None
+        self.max = max
+        self.bufsize = len(self.buffer)
+
+    def rollout(self, quantile=0.1):
+        return np.quantile(self.buffer, quantile)
+
+    def collect_rollout(self, buffer):
+        self.cutoff = buffer
 
     def __call__(self, smile, dockscore):
-        v = True
+        if len(self.buffer) <= self.max:
+            self.buffer.append(dockscore)
+            self.bufsize += 1
+        else:
+            self.buffer[random.randint(0, self.max)] = dockscore
 
-        if self.buffer_sat:
-            v =  dockscore <= np.quantile(self.buffer, 0.1)
+        if self.bufsize <= 5:
+            return True
+        elif self.cutoff is None:
+            return dockscore <= self.rollout()
+        else:
+            return dockscore <= self.cutoff
 
-        self.buffer[self.pos] = dockscore
-        self.pos = (self.pos + 1) % 10
-        if self.pos == 0:
-            self.buffer_sat = True
 
-        return v
+class MMGBSAPolicy:
+    def __init__(self):
+        self.cutoff = None
 
-def mmgbsa_ns_policy(smile, dock_score, minimize_score):
-    return False
+    def rollout(self):
+        return self.cutoff
+
+    def collect_rollet(self, buffer):
+        self.cutoff = buffer
+
+    def __call__(self, smile, dockscore):
+        if self.cutoff is not None:
+            return dockscore <= self.cutoff
+        else:
+            return True
--- runner.py
+++ runner.py
@@ -11,64 +11,65 @@ rank = comm.Get_rank()
 
 def setup_server():
     status_ = MPI.Status()
-    storage = {}
 
-    dockPolicy = policy.DockPolicy()
-    mmPolicy = policy.MinimizePolicy()
+    dockPolicy = policy.MasterDockPolicy()
+    mmPolicy = policy.MasterMinimizePolicy()
     print("Master setup server.")
     ts = time.time()
     ts_start = ts
+
     docked_count = 0
     param_count = 0
     while True:
-        
         data = comm.recv(source=MPI.ANY_SOURCE, tag=MPI.ANY_TAG, status=status_)
         if len(data) == 1: #pipeline 1
-            res = dockPolicy(*data)
-            comm.send(int(res), dest=status_.Get_source(), tag=11)
-            docked_count += 1
-        elif len(data) == 2: #pipeline 2
-            res = mmPolicy(*data)
-            comm.send(int(res), dest=status_.Get_source(), tag=11)
-            param_count += 1
-        elif len(data) == 3: #pipline 3
-            res = policy.mmgbsa_ns_policy(data[0], data[1], data[2])
-            comm.send(int(res), dest=status_.Get_source(), tag=11)
+            dtype = data.pop(0)
+            if dtype == 'dock':
+                r = dockPolicy(data[0])
+                comm.send(r, dest=status_.Get_source(), tag=11)
+            elif dtype == 'min': #pipeline 2
+                r = mmPolicy(data[0])
+                comm.send(r, dest=status_.Get_source(), tag=11)
+            elif dtype == 'mmgbsa': #pipline 3
+                pass
         else:
             print("got some weird data", data)
         if time.time() - ts > 15:
             ts = time.time()
             print("current counts", docked_count, param_count, time.time() - ts_start)
 
+
 def worker(df):
     size = comm.Get_size()
     struct = "input/"
     docker,recept = interface_functions.get_receptr()
 
+    dock_policy, min_policy = policy.DockPolicy(), policy.MinimizePolicy()
+
+    mols_docked = 0
+    mols_minimzed = 0
+
     for pos in range(rank - 1, df.shape[0], size - 1):
         try:
             path = "test" + str(pos)  + "/"
             smiles = df.iloc[pos,0]
-
-            # pipline
-            # comm.send([smiles], dest=0, tag=11)
-            # r = comm.recv(source=0, tag=11)
-            # print("Rank", rank, "should I run docking on", smiles,"?", "\t my model says", bool(r))
-            r = True
-            # pipeline
+            r = dock_policy(smiles)
             if r:
                 print("Rank", rank, "running docking...")
                 score = interface_functions.RunDocking_(smiles,struct,path, dock_obj=docker, write=True, recept=recept)
-                comm.send([smiles, score], dest=0, tag=11)
-                r = comm.recv(source=0, tag=11)
-                # print("Rank", rank, "should I run minimize, given the docking score", score, "?", "\t my model says", bool(r))
-                # r = True
-                # # pipeline
+                mols_docked += 1
+
+                if mols_docked % 1000 == 0:
+                    comm.send(['mini', min_policy.rollout()], dest=0, tag=11)
+                    r = comm.recv(source=0, tag=11)
+                    min_policy.collect_rollout(r)
+
+                r = min_policy(smiles, score)
+
                 if r:
-                    # print("Rank", rank, "running param and mini")
                     interface_functions.ParameterizeOE(path)
                     mscore = interface_functions.RunMinimization_(path, path)
-
+                    mols_minimzed += 1
                     # comm.send([smiles, score, mscore], dest=0, tag=11)
                     # r = comm.recv(source=0, tag=11)
                     # print("Rank", rank, "should I run mmgbsa for 1 ns given a energy minmization result of", mscore, "?\t my model says", bool(r))
