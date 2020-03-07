de269999633ea16f666d400ed7bcca240d6201dd
Austin Clyde <aclyde@uchicago.edu>
2020-03-06 20:18:07 -0600

    added subprocress for runner.py calls

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
-import pandas as pd
 import os
+
+import pandas as pd
+
 os.environ['OPENMM_CPU_THREADS'] = '1'
-import sys
-import policy
 from impress_md import interface_functions
 import time
-import subprocess
+import glob
 
 df = pd.read_csv("/Users/austin/Downloads/drug_bank.smi", sep=' ', header=None)
 
@@ -13,11 +13,9 @@ struct = "input/"
 docker, recept = interface_functions.get_receptr()
 
 start = time.time()
-for pos in range(0, 5):
-    print(pos)
-    path = "test" + str(pos)  + "/"
-    smiles = df.iloc[pos,0]
-    score = interface_functions.RunDocking_(smiles,struct,path, dock_obj=docker, recept=recept, write=True)
-    # interface_functions.ParameterizeOE(path)
-    # mscore = interface_functions.RunMinimization_(path, path)
+
+files = glob.glob("/Users/austin/Downloads/good_pl/rank*")
+for file in files:
+    interface_functions.ParameterizeOE(file + "/")
+
 print(time.time() - start)
