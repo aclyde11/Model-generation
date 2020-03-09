5e49e35bbe690bbfe17037218354379eb8fcad4e
Austin Clyde <aclyde@uchicago.edu>
2020-03-09 17:00:13 -0500

    added example

--- theta_dock.py
+++ theta_dock.py
@@ -2,19 +2,37 @@ import sys
 import pandas as pd
 from impress_md import interface_functions
 
+
 if __name__ == '__main__':
-    file_name = sys.argv[1]
-    ress = ""
-    dock_obj = None
-    with open(file_name, 'r') as f:
-        for line in f:
-            smiles, receptor_file, path, dbase_name, target_name, pos, name = line.split(',')
-            if dock_obj is None:
-                dock_obj, receptor = interface_functions.get_receptr(receptor_file)
-            name = name.strip()
-            _, res = interface_functions.RunDocking_(smiles, receptor_file, path, dbase_name, target_name,
+    smiles_files = pd.read_csv(sys.argv[1], sep=' ', header=None)
+    target_filoe = sys.argv[2]
+    dbase_name = 'test'
+    target_name = 'pl_pro'
+
+    WHICH_OPTION=0
+    ### OPTION 1, no need to read receptor from file system everytime
+
+    if WHICH_OPTION:
+        docker, receptor = interface_functions.get_receptr(target_filoe)
+
+        for pos in range(smiles_files.shape[0]):
+            smiles = smiles_files.iloc[pos, 0]
+            ligand_name = smiles_files.iloc[pos, 1]
+            score, res = interface_functions.RunDocking_(smiles, None, None, dbase_name, target_name,
                                             pos=pos, write=True,
-                                            receptor_file=receptor_file, name=name,
-                                            docking_only=True, dock_obj=dock_obj)
-            ress += res
-    print(ress)
+                                            receptor_file=None, name=ligand_name,
+                                            docking_only=True, dock_obj=docker, recept=receptor)
+            print(res)
+
+
+    ### OPTION 2, need to read receptor eevery time
+    else:
+
+        for pos in range(smiles_files.shape[0]):
+            smiles = smiles_files.iloc[pos, 0]
+            ligand_name = smiles_files.iloc[pos, 1]
+            score, res = interface_functions.RunDocking_(smiles, target_filoe, None, dbase_name, target_name,
+                                                         pos=pos, write=True,
+                                                         receptor_file=target_filoe, name=ligand_name,
+                                                         docking_only=True, dock_obj=None, recept=None)
+            print(res)
