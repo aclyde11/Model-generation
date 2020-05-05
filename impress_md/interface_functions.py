66f510fd863bf0e3679b71c71d2459d396a734a8
Austin Clyde <aclyde@uchicago.edu>
2020-05-04 20:13:42 -0500

    updtaed readme

--- impress_md/interface_functions.py
+++ impress_md/interface_functions.py
@@ -145,7 +145,10 @@ def RunDocking_(smiles: str, dock_obj: oedocking.OEDock, pos: int = None, name: 
 
         dockMethod = dock_obj.GetName()
         oedocking.OESetSDScore(ligand_min, dock_obj, dockMethod)
-        ligand_min.SetTitle(name)
+        try:
+            ligand_min.SetTitle(name)
+        except TypeError:
+            print("weird name error")
     else:
         return None, None, None
 
