2ebbe3efed7398cfa28596f11118995850ce1da4
Austin Clyde <aclyde@uchicago.edu>
2020-05-13 13:09:43 -0500

    updtaed readme

--- fastrocsconfgen.py
+++ fastrocsconfgen.py
@@ -1,6 +1,7 @@
 from openeye import oechem
 from openeye import oeomega
 import argparse
+from openeye import oemolprop
 
 def gen_conf(mol):
     oemols = []
@@ -17,6 +18,34 @@ def gen_conf(mol):
                                    (enantiomer.GetTitle(), oeomega.OEGetOmegaError(ret_code)))
     return oemols
 
+# def filter_smiles(smis):
+#     '''
+#     :param smis: list of smiles
+#     :return: (oe graph mols, smiles)
+#     '''
+#     ims = oechem.oemolistream()
+#     ims.SetFormat(oechem.OEFormat_SMI)
+#     smiles = "\n".join(list(smis))
+#     ims.openstring(smiles)
+#
+#     oms = oechem.oemolostream()
+#     oms.SetFormat(oechem.OEFormat_SMI)
+#     oms.openstring()
+#
+#     filt = oemolprop.OEFilter(oemolprop.OEFilterType_BlockBuster)
+#
+#     goods = []
+#     for i, mol in enumerate(ims.GetOEGraphMols()):
+#         if filt(mol):
+#             oechem.OEWriteMolecule(oms, mol)
+#             goods.append(i)
+#     actions = str(oms.GetString().decode("utf-8"))
+#     actions = actions.split("\n")
+#
+#     oms.close()
+#     ims.close()
+#     return [smis[i] for i in goods], actions
+
 def from_smiles(in_smiles, outfile):
     '''
 
@@ -32,13 +61,16 @@ def from_smiles(in_smiles, outfile):
 
     ofs = oechem.oemolostream()
     ofs.SetFormat(oechem.OEFormat_OEB)
+    filt = oemolprop.OEFilter(oemolprop.OEFilterType_BlockBuster)
+
     if not ofs.open(outfile):
         oechem.OEThrow.Fatal("Unable to open %s for writing" % outfile)
 
     for mol in ims.GetOEMols():
-        result = gen_conf(mol)
-        for res in result:
-            oechem.OEWriteMolecule(ofs, res)
+        if filt(mol):
+            result = gen_conf(mol)
+            for res in result:
+                oechem.OEWriteMolecule(ofs, res)
 
     ofs.close()
     return 0
@@ -51,10 +83,11 @@ def from_single_smiles(in_smiles, outfile):
     ofs.SetFormat(oechem.OEFormat_OEB)
     if not ofs.open(outfile):
         oechem.OEThrow.Fatal("Unable to open %s for writing" % outfile)
-
-    result = gen_conf(mol)
-    for res in result:
-        oechem.OEWriteMolecule(ofs, res)
+    filt = oemolprop.OEFilter(oemolprop.OEFilterType_BlockBuster)
+    if filt(mol):
+        result = gen_conf(mol)
+        for res in result:
+            oechem.OEWriteMolecule(ofs, res)
 
     ofs.close()
     return 0
