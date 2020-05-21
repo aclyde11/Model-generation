25082ac999f17d86aec06eb8084fb887bd54fe4f
Austin Clyde <aclyde@uchicago.edu>
2020-05-21 18:58:32 -0500

    updated from rick

--- sdsort.py
+++ sdsort.py
@@ -1,6 +1,7 @@
-from openeye import oechem
 import argparse
+
 import numpy as np
+from openeye import oechem
 from tqdm import tqdm
 
 
@@ -10,7 +11,7 @@ def getargs():
     parser.add_argument('-o', type=str, required=True)
     parser.add_argument('-t', type=str, required=True)
     parser.add_argument('-u', action='store_true')
-    parser.add_argument('-v', default=1, type=int, choices=[0,1,2])
+    parser.add_argument('-v', default=1, type=int, choices=[0, 1, 2])
     parser.add_argument('-n', default=-1, type=int)
 
     return parser.parse_args()
@@ -32,29 +33,33 @@ if __name__ == '__main__':
         pbar = tqdm(desc='reading input {}'.format(args.i))
     while oechem.OEReadMolecule(ifs, mol):
         mols.append(oechem.OEMol(mol))
-        scores.append(float(oechem.OEGetSDData(mol, args.t)))
+        try:
+            scores.append(float(oechem.OEGetSDData(mol, args.t)))
+        except ValueError:
+            scores.append(0.0)
+            print("ValueError -- substituted 0.0")
+        #            break
 
         if args.v >= 1:
             pbar.update(1)
 
-    if args.v >=1:
+    if args.v >= 1:
         pbar.close()
     ifs.close()
 
-    if args.v >=1:
+    if args.v >= 1:
         print("sorting...")
     sort_order = np.argsort(scores)
 
-    for i in tqdm(sort_order,desc=('writing out {}' if not args.u else 'writing unique molname out {}').format(args.o)):
-        if args.n != -1 and args.i >= args.n:
-            break
-
-        if args.u and mols[i].GetTitle() not in wrote_titles:
-            oechem.OEWriteMolecule(ofs, mols[i])
-            wrote_titles[mols[i].GetTitle()] = 1
-        elif not args.u:
-            oechem.OEWriteMolecule(ofs, mols[i])
-        elif args.u and args.v >= 2:
-            print("[-u] skipping ", mols[i].GetTitle())
+    for i in tqdm(sort_order,
+                  desc=('writing out {}' if not args.u else 'writing unique molname out {}').format(args.o)):
+        if (args.n == -1) or (i <= args.n):
+            if args.u and mols[i].GetTitle() not in wrote_titles:
+                oechem.OEWriteMolecule(ofs, mols[i])
+                wrote_titles[mols[i].GetTitle()] = 1
+            elif not args.u:
+                oechem.OEWriteMolecule(ofs, mols[i])
+            elif args.u and args.v >= 2:
+                print("[-u] skipping ", mols[i].GetTitle())
 
     ofs.close()
